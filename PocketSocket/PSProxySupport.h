1e7dd5ab6e803bf5920c85d749f86fd73d70cfe6
Matthias Keiser <matthias@tristan-inc.com>
2021-02-23 12:11:12 +0100

    update a comment #35701

--- PocketSocket/PSProxySupport.h
+++ PocketSocket/PSProxySupport.h
@@ -7,6 +7,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PSProxySupport : NSObject
 /// Reads the system proxy settings and applies the first suitable proxy to the stream.
+/// This is not perfect, it would be better to not just try the first proxy in the list, but try each one until one works. However it is
+/// not clear what would be the error conditions on which one tries the next proxy, vs just a "regular" network problem.
 /// Note: this method might make a synchronous network call, so it should only be called on a background thread.
 + (void)applySuitableProxyForURL:(NSURL *)url toStream:(CFReadStreamRef)stream;
 @end
